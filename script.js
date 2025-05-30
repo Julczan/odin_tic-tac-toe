67d2d7651299ef94767bb171cf7aa83d1c2b8551
Julczan <julek111222h@gmail.com>
2025-05-30 22:04:27 +0200

    updating board value

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("elo");
+const Gameboard = (function () {
+  const board = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+
+  const updateBoard = function (row, column, value) {
+    board[row][column] = value;
+    return board;
+  };
+
+  const getBoard = () => board;
+  return { getBoard, updateBoard };
+})();
+
+function GameController(playerOneName, playerTwoName) {
+  const players = [
+    {
+      name: playerOneName,
+      token: 1,
+    },
+
+    {
+      name: playerTwoName,
+      token: 2,
+    },
+  ];
+}
+
+function changeVal() {
+  console.log(Gameboard.updateBoard(1, 1, 13));
+}
+
+changeVal();

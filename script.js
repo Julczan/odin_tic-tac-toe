458fabcb1ceba11878f2ae45883f3efa416d37d9
Julczan <julek111222h@gmail.com>
2025-06-04 15:00:04 +0200

    new draw conditions. to do: fix winning conditions

--- script.js
+++ script.js
@@ -1,170 +1,103 @@
 const Gameboard = (function () {
-  let board = [];
-
-  const drawConditions = () => {
-    const allCellsTaken =
-      board[0][0].getValue() !== 0 &&
-      board[0][1].getValue() !== 0 &&
-      board[0][2].getValue() !== 0 &&
-      board[1][0].getValue() !== 0 &&
-      board[1][1].getValue() !== 0 &&
-      board[1][2].getValue() !== 0 &&
-      board[2][0].getValue() !== 0 &&
-      board[2][1].getValue() !== 0 &&
-      board[2][2].getValue() !== 0;
-
-    if (allCellsTaken) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-
-  const winningConditions = () => {
-    const row1WinningCondition =
-      board[0][0].getValue() === board[0][1].getValue() &&
-      board[0][1].getValue() === board[0][2].getValue() &&
-      board[0][1].getValue() !== 0;
-
-    const row2WinningCondition =
-      board[1][0].getValue() === board[1][1].getValue() &&
-      board[1][1].getValue() === board[1][2].getValue() &&
-      board[1][1].getValue() !== 0;
-
-    const row3WinningCondition =
-      board[2][0].getValue() === board[2][1].getValue() &&
-      board[2][1].getValue() === board[2][2].getValue() &&
-      board[2][1].getValue() !== 0;
-
-    const column1WinningCondition =
-      board[0][0].getValue() === board[1][0].getValue() &&
-      board[1][0].getValue() === board[2][0].getValue() &&
-      board[1][0].getValue() !== 0;
-
-    const column2WinningCondition =
-      board[0][1].getValue() === board[1][1].getValue() &&
-      board[1][1].getValue() === board[2][1].getValue() &&
-      board[0][1].getValue() !== 0;
-
-    const column3WinningCondition =
-      board[0][2].getValue() === board[1][2].getValue() &&
-      board[1][2].getValue() === board[2][2].getValue() &&
-      board[0][2].getValue() !== 0;
-
-    const diagonalCondition =
-      board[0][0].getValue() === board[1][1].getValue() &&
-      board[1][1].getValue() === board[2][2].getValue() &&
-      board[0][0].getValue() !== 0;
-
-    const contrDiagonalCondition =
-      board[2][0].getValue() === board[1][1].getValue() &&
-      board[1][1].getValue() === board[0][2].getValue() &&
-      board[2][0].getValue() !== 0;
-
-    if (
-      row1WinningCondition ||
-      row2WinningCondition ||
-      row3WinningCondition ||
-      column1WinningCondition ||
-      column2WinningCondition ||
-      column3WinningCondition ||
-      diagonalCondition ||
-      contrDiagonalCondition
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-
-  for (let i = 0; i < 3; i++) {
-    board[i] = [];
-    for (let j = 0; j < 3; j++) {
-      board[i].push(Cell());
-    }
-  }
+  let board = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
 
   const dropToken = (row, column, player) => {
-    const availableCells = board.map((row) =>
-      row.map((cell) => cell.getValue() === 0)
-    );
+    const availableCells = board.map((row) => row.map((cell) => cell === 0));
+
     if (availableCells[row][column]) {
-      board[row][column].addToken(player);
+      board[row][column] = player;
     } else {
       return;
     }
   };
 
-  const printBoard = () => {
-    const boardWithCellValues = board.map((row) =>
-      row.map((cell) => cell.getValue())
-    );
-
-    console.log(boardWithCellValues);
-  };
-
   const getBoard = () => board;
 
-  return { getBoard, dropToken, printBoard, winningConditions, drawConditions };
+  return { getBoard, dropToken };
 })();
 
-function Cell() {
-  let value = 0;
+function gameConditions(board) {
+  // const winningCombinations = [
+  //   [0, 1, 2],
+  //   [3, 4, 5],
+  //   [6, 7, 8],
+  //   [0, 3, 6],
+  //   [1, 4, 7],
+  //   [2, 5, 8],
+  //   [0, 4, 8],
+  //   [2, 4, 6],
+  // ];
+  // for (let i = 0; i < winningCombinations.length; i++) {
+  //   const [a, b, c] = winningCombinations[i];
+  //   if (board[a] !== 0 && board[a] === board[b] && board[a] === board[c]) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+  const drawConditions = (board) => {
+    const checkRow1 = board[0].some((v) => v === 0);
+    const checkRow2 = board[1].some((v) => v === 0);
+    const checkRow3 = board[2].some((v) => v === 0);
 
-  const addToken = (player) => {
-    value = player;
+    if (!checkRow1 && !checkRow2 && !checkRow3) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
-  const getValue = () => value;
-
-  return { addToken, getValue };
+  return { drawConditions };
 }
 
-function gameFlow(playerOneName = "Player One", playerTwoName = "Player Two") {
-  const board = Gameboard;
-
+function createPlayer(
+  playerOneName = "Player one",
+  playerTwoName = "Player Two"
+) {
   const players = [
-    {
-      name: playerOneName,
-      token: 1,
-    },
-    {
-      name: playerTwoName,
-      token: 2,
-    },
+    { name: playerOneName, token: 1 },
+    { name: playerTwoName, token: 2 },
   ];
 
   let activePlayer = players[0];
 
-  const changePlayerTurn = function () {
+  const changePlayerTurn = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
   };
 
   const getActivePlayer = () => activePlayer;
 
+  return { getActivePlayer, changePlayerTurn };
+}
+
+function gameFlow() {
+  const board = Gameboard;
+  const players = createPlayer();
+  const condition = gameConditions();
+
   const printNewRound = () => {
-    board.printBoard();
-    console.log(`${getActivePlayer().name}'s turn`);
+    console.log(board.getBoard());
+    console.log(`${players.getActivePlayer().name}'s turn`);
   };
 
   const playRound = (column, row) => {
-    board.dropToken(row, column, getActivePlayer().token);
+    board.dropToken(row, column, players.getActivePlayer().token);
 
-    if (board.drawConditions()) {
-      console.log("It's a DRAW!");
+    if (condition.drawConditions(board.getBoard())) {
+      console.log("ITS A DRAW");
     }
 
-    if (board.winningConditions()) {
-      console.log("WINNER!");
-    } else {
-      changePlayerTurn();
-      printNewRound();
-    }
+    players.changePlayerTurn();
+    printNewRound();
   };
 
   printNewRound();
 
-  return { playRound, getActivePlayer };
+  return { playRound };
 }
 
 const game = gameFlow();
